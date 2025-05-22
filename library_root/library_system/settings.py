470376613ffc175cc02d3bb34a65a3b44292e8b3
marie-latras <pearlmarielatras@gmail.com>
2025-05-22 14:34:50 +0800

    Update settings.py

--- library_root/library_system/settings.py
+++ library_root/library_system/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ipoc9b8bi02%uapiuaxfqsu2&wp@z5#dcs2554r-)*7xl-e&0c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
